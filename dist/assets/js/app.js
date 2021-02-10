8a90d42c0f59fbfaa762893069288fa9d2dd260a
Marat Minchik <devminrat@gmail.com>
2021-02-10 19:38:16 +0300

    added question section

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,7 +1,22 @@
 // example --  //= ../../../node_modules/slick-carousel/slick/slick.js
 
 $(function () {
-  // example --   //= components/slick.js
+  // Accordeon questions section
 
-  console.log("file 3");
+  $(".questions__accordeon-content").hide();
+
+  $(".questions__accordeon-title").click(function () {
+    // $(this).parent().toggleClass("active").siblings().removeClass("active");
+    $(".questions__accordeon-content").slideUp();
+
+    if (!$(this).next().is(":visible")) {
+      $(this).next().slideDown();
+    }
+  });
+
+  $(".question__button-more").click(function () {
+    $(".hidden-item").toggleClass("accordeon-item--hidden");
+    $(".show-more").toggleClass("show-more--hidden");
+    $(".show-less").toggleClass("show-less--hidden");
+  });
 });
