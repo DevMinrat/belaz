d9fb16b1d904b2dc06231260861a15463559edbf
Marat Minchik <devminrat@gmail.com>
2021-02-18 12:47:28 +0300

    adaptived header and added arrow to top

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -21,5 +21,59 @@ $(function () {
     $(".show-less").toggleClass("show-less--hidden");
   });
 
+  // to top arrow
+
+  var arrow = $("#top-arrow"),
+    introH = $("#intro").innerHeight(),
+    scrollOffset = $(window).scrollTop();
+
+  checkScroll(scrollOffset);
+
+  $(window).on("scroll", function () {
+    scrollOffset = $(this).scrollTop();
+
+    checkScroll(scrollOffset);
+  });
+
+  function checkScroll(scrollOffset) {
+    if (scrollOffset >= introH) {
+      arrow.addClass("top-arrow--visible");
+    } else {
+      arrow.removeClass("top-arrow--visible");
+    }
+  }
+
+  //burger-menu
+
+  $(".burger-menu").click(function () {
+    $(this).toggleClass("menu-on");
+    $("#nav").toggleClass("nav-mobile");
+    $(".header-info").toggleClass("info-mobile");
+  });
+
+  $(window).scroll(function () {
+    $(".burger-menu").removeClass("menu-on");
+    $("#nav").removeClass("nav-mobile");
+    $(".header-info").removeClass("info-mobile");
+  });
+
+  $("[data-scroll]").on("click", function (event) {
+    event.preventDefault();
+
+    var $this = $(this),
+      blockId = $this.data("scroll"),
+      blockOffset = $(blockId).offset().top;
+
+    $("#nav a").removeClass("active");
+    $this.addClass("active");
+
+    $("html, body").animate(
+      {
+        scrollTop: blockOffset,
+      },
+      500
+    );
+  });
+
   //= components/swiper.js
 });
